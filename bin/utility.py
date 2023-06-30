2bc43090de5be7554b46b477dfc9aa4ba6f03412
gjanders <gareth.anderson03@gmail.com>
2023-06-30 16:04:42 +1000

    Update utility.py

--- bin/utility.py
+++ bin/utility.py
@@ -8,12 +8,17 @@ def determine_username(service_obj):
       Run a get request to determine the username and roles
     """
     res = service_obj.get(path_segment='/services/authentication/current-context', output_mode='json')
-    #res['body'] is a response writer but I am unable to determine the nice way to access the body of the data, so str works...    
+    #res['body'] is a response writer but I am unable to determine the nice way to access the body of the data, so str works...
     json_dict = json.loads(res['body'].read().decode("utf-8"))
     username = json_dict['entry'][0]['content']['username']
     roles = json_dict['entry'][0]['content']['roles']
+    capabilities = json_dict['entry'][0]['content']['capabilities']
 
-    return (username, roles)
+    has_admin = False
+    if 'admin_all_objects' in capabilities:
+        has_admin = True
+
+    return username, roles, has_admin
 
 def determine_write(service_obj, app_name):
     """
@@ -21,7 +26,9 @@ def determine_write(service_obj, app_name):
       Run a get request to determine if the user running the command has write access to
       the requested app. Return True/False for write access and the username of the current user
     """
-    (username, roles) = determine_username(service_obj)
+    username, roles, has_admin = determine_username(service_obj)
+    if has_admin:
+        return True, username
 
     res = service_obj.get(path_segment='/servicesNS/nobody/system/apps/local/' + app_name, output_mode='json')
     json_dict = json.loads(res['body'].read().decode("utf-8"))
@@ -33,4 +40,4 @@ def determine_write(service_obj, app_name):
         if set(write_roles) & set(roles):
           has_write = True
 
-    return (has_write, username)
+    return has_write, username
